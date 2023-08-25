e3ae8a7cb5b52d1dbe8a4f59393e903ca4fe5fa3
donishyar <ahmadshahdonishyar@gmail.com>
2023-08-25 11:06:29 +0430

    working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-
+import {BrowserRouter} from 'react-dom';
+import {About,Contact,Experience,Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas} from './components'
 const App = () => {
   
   return (
-    <div>
-      3D Developer Portfolio
-    </div>
+    <BrowserRouter>
+    
+    </BrowserRouter>
   )
 }
 
